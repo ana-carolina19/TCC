a43ffae88fc45165254b899f3626e94fbec461d7
emanuel santoss <emanuelsantoss42@gmail.com>
2022-12-18 02:23:02 -0300

    Adicionado as cores e iniciando o layout

--- ChamaAi/src/app/app-routing.module.ts
+++ ChamaAi/src/app/app-routing.module.ts
@@ -4,7 +4,20 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
+    redirectTo: 'login',
+    pathMatch:'full',
+  },
+  {
+    path: 'tabs',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'registro',
+    loadChildren: () => import('./registro/registro.module').then( m => m.RegistroPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   }
 ];
 @NgModule({
