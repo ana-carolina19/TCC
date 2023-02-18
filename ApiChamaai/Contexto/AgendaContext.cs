9825655b51e8e4c53aa9b84e851d521f4693d113
Ana Carolina Antonio de Sousa <carolisousa102016@gmail.com>
2023-02-17 22:24:45 -0300

    100

---
+++ ApiChamaai/Contexto/AgendaContext.cs
@@ -0,0 +1,20 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiChamaai.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace ModuloAPI.Context
+{
+    public class AgendaContext : DbContext /*Ela esta herdando as funções do EntityFrameWork*/
+    {
+        public AgendaContext(DbContextOptions<AgendaContext> options) : base(options){ /*O contexto server para receber a conexão com o banco*/
+
+        }
+
+        public DbSet<Contato> Contatos { get; set; } /*O DbSet server criar uma tabela com a migration*/
+    }
+}
